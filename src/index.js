8586a05a5723a870caa24b2002c87e1071eed085
zhony94 <64091893+zhony94@users.noreply.github.com>
2022-02-21 16:36:40 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -36,10 +36,38 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
+let holder = []
+let result = ''
 
 function decode(expr) {
-    // write your solution here
+    for (let i = 0; i<expr.length/10; i++){
+  holder.push(expr.slice(i*10,i*10+10))
 }
+
+for(let i=0; i<holder.length; i++){
+  if (holder[i]==='**********'){
+    result = result + ' '
+  }else{
+    let encodedWord = wordDecode(holder[i])
+    result = result + MORSE_TABLE[encodedWord]
+  }
+}return result
+}
+
+
+function wordDecode(encoded){
+  let word = ''
+  for (let i=0; i<5;i++){
+    if(encoded.slice(i*2,i*2+2)==='00'){
+      word = word + '';
+    }else if(encoded.slice(i*2,i*2+2)==='10'){
+      word = word + '.'
+    }else if(encoded.slice(i*2,i*2+2)==='11'){
+      word = word + '-'
+    }
+  }return word
+}
+
 
 module.exports = {
     decode
